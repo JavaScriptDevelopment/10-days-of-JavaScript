2948a07efd4691c4a772ac41321224af7e722fff
A.Rupesh <a.rupesh66@gmail.com>
2018-08-24 20:03:04 +0530

    final commit

---
+++ day-4/Count Objects.js
@@ -0,0 +1,12 @@
+function getCount(objects) {
+    var count = 0;
+    for(var index in objects){
+    if(objects[index].x == objects[index].y)
+{
+    count++;
+}
+ 
+    }
+    return  count;
+    
+}
