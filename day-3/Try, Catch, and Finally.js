fe44a3e1b0f383a0cea0cff8aa264b69ff3e522e
A.Rupesh <a.rupesh66@gmail.com>
2018-08-24 19:46:46 +0530

    second commit

---
+++ day-3/Try, Catch, and Finally.js
@@ -0,0 +1,10 @@
+
+function reverseString(s) {
+    try {
+        s = s.split('').reverse().join('');
+        console.log(s);
+    }catch(e){
+        console.log(e.message)
+        console.log(s)
+    }
+}
